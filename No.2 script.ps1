07a46c2e542e132c24fb2c90f94c18d9c99c08da
Olamide <145977590+0xSp0rt@users.noreply.github.com>
2024-08-27 12:33:15 -0400

    Add files via upload

---
+++ No.2 script.ps1
@@ -0,0 +1,7 @@
+﻿New-VMSwitch -Name 'OlamideS.Privateswitch' -SwitchType 'Private'
+New-VM -Name 'ServerOS.2022' -MemoryStartupBytes 4096MB -Generation 2 -Path 'C:\Programdata\Microsoft\Windows\Hyper-V' -SwitchName 'OlamideS.Privateswitch'
+New-VHD -Path 'C:\Programdata\Microsoft\Windows\Virtual Hard Disks\ServerOS.2022.vhdx' -SizeBytes 60GB -Dynamic\
+Add-VMHardDiskDrive -VMName 'ServerOS.2022' -Path 'C:\Programdata\Microsoft\Windows\Virtual Hard Disks\ServerOS.2022.vhdx'
+Add-VMDvdDrive -VMName 'ServerOS.2022' -Path 'C:\Users\samue\OneDrive\Desktop\20348.169.210806-2348.fe_release_svc_refresh_SERVER_EVAL_x64FRE_en-us.iso'
+Set-VMFirmware -VMName 'ServerOS.2022' -EnableSecureBoot Off
+Set-VMProcessor -VMName 'ServerOS.2022' -Count 4
